937a5dc50e72f76fe3955a5bd00484ac3418cefc
Joe Ringenberg <joe@wistia.com>
2013-08-23 15:13:42 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplimentSandwiches::Application.config.secret_token = '9f15dd452ed3864a7e1cbf0676dbd9cec5a45e91efc1c4c995c2072aac0d7bc9a9842dd8aae35a741fb376c285a0cb3b34e667b2695aee11b392a78b51cf3be1'
